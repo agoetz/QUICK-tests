e31b316972eb66f7664c0cf0256583021e0a5532
Andreas Goetz <agoetz@sdsc.edu>
2025-06-03 13:41:25 -0700

    Add morphine htc timings

---
+++ benchmarks/QUICK-25-dev-ebc007d8/MI300A-Cosmos/htc/run-htc.sh
@@ -0,0 +1,16 @@
+input=morphine.in
+bench=run-5-morphine
+job=${bench}.sh
+log=${bench}.log
+echo ">>> Running on host $host <<<"
+echo " >> Benchmark $bench"
+for i in 1 2 3 4
+do
+    echo " >> launch iteration $i"
+    mkdir $i
+    cp $input $i/
+    cp $job $i/
+    pushd $i
+    ./$job > $log &
+    popd
+done
